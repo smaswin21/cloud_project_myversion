70f17ec1af62535b790c6a2a0e6b282bec122b34
Ronald <rbeltran076@gmail.com>
2023-11-26 20:13:30 +0100

    First commit

--- frontend.py
+++ frontend.py
@@ -1,38 +1,43 @@
 import streamlit as st
 
+# HEAD ----------------------------
 st.set_page_config(
     page_title="Valve Dashboard",
-
     page_icon="👋",
 )
 
+# BODY ----------------------------
 st.title('Valve Control Dashboard')
 
-# Assuming you have a function to get the current status of the valve
-def get_valve_status(valve_id, command):
-    # This function should interact with your backend to get the current status
-    # For now, let's just return a placeholder
-    return command
+# declare the variables for valve status
+valves = {
+    'Valve 1': 'closed', 
+    'Valve 2': 'closed', 
+    'Valve 3': 'closed', 
+    }
 
 # Function to send command to the backend
-def send_valve_command(valve_id, command):
+def send_valve_command(valveId, command):
     # Here you would add the logic to send a command to your backend,
     # which would then communicate with Azure IoT Hub
     pass
 
-valve_id = st.selectbox('Select Valve', ['Valve 1', 'Valve 2', 'Valve 3'])
+valveId = st.selectbox('Select Valve', ['Valve 1', 'Valve 2', 'Valve 3'])
+
 
 if st.button('Open Valve'):
-    send_valve_command(valve_id, 'open')
+    send_valve_command(valveId, 'open')
+    # set the corresponding valve to open
+    valves[valveId] = 'open'
     st.success('Command sent to open the valve!')
 
 if st.button('Close Valve'):
-    send_valve_command(valve_id, 'close')
+    send_valve_command(valveId, 'closed')
+    # set the corresponding valve to closed
+    valves[valveId] = 'closed'
     st.success('Command sent to close the valve!')
 
-st.write(f'Status: {get_valve_status(valve_id)}')
-
-# st.sidebar.success("Select a demo above.")
 
 
-
+# print the status of the corresponding valve
+st.write(f'Status: {valves[valveId]}')
